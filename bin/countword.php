9d41bf1ff071bd12d398b49deb66a0d19a200f23
Petr Karmashev <smonkl@bk.ru>
2017-01-24 13:10:25 +0300

    Update countword.php

--- bin/countword.php
+++ bin/countword.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$command = new \Vehsa\UniqueWordsCounter\CountWordsCommand();
+$command = new Vehsa\UniqueWordsCounter\CountWordsCommand();
 
-$application = new \Vehsa\UniqueWordsCounter\Application($argv);
+$application = new Vehsa\UniqueWordsCounter\Application($argv);
 $application->run($command);
